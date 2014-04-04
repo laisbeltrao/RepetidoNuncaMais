4b9c5d282c77fa55016ca433e1b7c8cd63b86a69
Laís Beltrão <laiscbeltrao@gmail.com>
2014-04-04 18:00:44 -0300

    um

---
+++ RepetidoNuncaMais/gen/br/aeso/android/repetidonuncamais/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.aeso.android.repetidonuncamais;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ RepetidoNuncaMais/src/br/aeso/android/repetidonuncamais/Index.java
@@ -0,0 +1,22 @@
+package br.aeso.android.repetidonuncamais;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Index extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_index);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.index, menu);
+		return true;
+	}
+
+}
